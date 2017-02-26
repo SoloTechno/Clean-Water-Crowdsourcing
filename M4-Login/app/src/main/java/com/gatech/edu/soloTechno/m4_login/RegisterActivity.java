d58d593012479b071e706c593b4a8ff2ce438be3
Sung Jae Hong <sroyhong313@gmail.com>
2017-02-25 21:09:05 -0500

    init firebase

--- M4-Login/app/src/main/java/com/gatech/edu/soloTechno/m4_login/RegisterActivity.java
+++ M4-Login/app/src/main/java/com/gatech/edu/soloTechno/m4_login/RegisterActivity.java
@@ -2,12 +2,20 @@ package com.gatech.edu.soloTechno.m4_login;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,10 +27,11 @@ import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity {
     private Spinner accountTypeSpinner;
-    private EditText firstName;
-    private EditText lastName;
-    private EditText email;
-    private EditText password;
+    private EditText firstName_text;
+    private EditText lastName_text;
+    private EditText email_text;
+    private EditText password_text;
+    private static FirebaseAuth auth;
 
 
     public static List<String> accounts = Arrays.asList("Manager", "Worker", "Admin", "User");
@@ -31,26 +40,82 @@ public class RegisterActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-
         setContentView(R.layout.activity_register);
 
+        accountTypeSpinner = (Spinner) findViewById(R.id.spinner4);
+        firstName_text = (EditText) findViewById(R.id.first_Name);
+        lastName_text = (EditText) findViewById(R.id.last_Name);
+        email_text = (EditText) findViewById(R.id.email);
+        password_text = (EditText) findViewById(R.id.password);
+
+        auth = FirebaseAuth.getInstance();
         final Button saveButton = (Button) findViewById(R.id.save_button);
         saveButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent saveActivity = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(saveActivity);
+                submitForm();
             }
         });
 
-        accountTypeSpinner = (Spinner) findViewById(R.id.spinner4);
-        firstName = (EditText) findViewById(R.id.first_Name);
-        lastName = (EditText) findViewById(R.id.last_Name);
-        email = (EditText) findViewById(R.id.email);
-        password = (EditText) findViewById(R.id.password);
+
 
         ArrayAdapter<String> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, accounts);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         accountTypeSpinner.setAdapter(adapter);
 
     }
+    private void submitForm() {
+
+        String email = email_text.getText().toString().trim();
+        String password = password_text.getText().toString().trim();
+        String firstName = firstName_text.getText().toString().trim();
+        String lastName = lastName_text.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //create user
+        auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException(),
+                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            startActivity(intent);
+                        }
+                    }
+                });
+
+    }
+//});
+//        }
+//
+//        auth.createUserWithEmailAndPassword(email, password)
+//                .addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if (task.isSuccessful()) {
+//                            Toast.makeText(RegisterActivity.this, "Registered Successfully" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+//                            Intent saveActivity = new Intent(getApplicationContext(), MainActivity.class);
+//                            startActivity(saveActivity);
+//                        } else if (!task.isSuccessful()) {
+//                            Toast.makeText(RegisterActivity.this, "Nope Successfully" + task.isSuccessful(), Toast.LENGTH_SHORT).show();
+//                            return;
+//                        }
+//                    }
+//                });
+//        Toast.makeText(getApplicationContext(), "You are successfully Registered !!", Toast.LENGTH_SHORT).show();
+//    }
 }
