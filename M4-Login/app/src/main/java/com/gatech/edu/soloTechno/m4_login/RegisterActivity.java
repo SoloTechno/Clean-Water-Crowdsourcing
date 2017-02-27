5a6a1438c00657fb8629ec51ed32975e79aabf9a
Timothy Baba <timothybaba@lawn-143-215-115-21.lawn.gatech.edu>
2017-02-26 21:04:17 -0500

    Completed database storage and provided an authorization state listener

--- M4-Login/app/src/main/java/com/gatech/edu/soloTechno/m4_login/RegisterActivity.java
+++ M4-Login/app/src/main/java/com/gatech/edu/soloTechno/m4_login/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -16,9 +17,14 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
 import java.util.List;
+
+import static android.R.attr.name;
 
 
 /**
@@ -31,7 +37,16 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText lastName_text;
     private EditText email_text;
     private EditText password_text;
-    private static FirebaseAuth auth;
+    private FirebaseAuth auth;
+    public static final String TAG = RegisterActivity.class.getSimpleName();
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private String accountType;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+
 
 
     public static List<String> accounts = Arrays.asList("Manager", "Worker", "Admin", "User");
@@ -41,6 +56,8 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle icicle) {
         super.onCreate(icicle);
         setContentView(R.layout.activity_register);
+
+        auth = FirebaseAuth.getInstance();
 
         accountTypeSpinner = (Spinner) findViewById(R.id.spinner4);
         firstName_text = (EditText) findViewById(R.id.first_Name);
@@ -62,13 +79,16 @@ public class RegisterActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         accountTypeSpinner.setAdapter(adapter);
 
+        createAuthStateListener();
+
     }
     private void submitForm() {
 
-        String email = email_text.getText().toString().trim();
-        String password = password_text.getText().toString().trim();
-        String firstName = firstName_text.getText().toString().trim();
-        String lastName = lastName_text.getText().toString().trim();
+        accountType = accountTypeSpinner.getSelectedItem().toString().trim();
+        email = email_text.getText().toString().trim();
+        password = password_text.getText().toString().trim();
+        firstName = firstName_text.getText().toString().trim();
+        lastName = lastName_text.getText().toString().trim();
 
         if (TextUtils.isEmpty(email)) {
             Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
@@ -92,12 +112,54 @@ public class RegisterActivity extends AppCompatActivity {
                             Toast.makeText(RegisterActivity.this, "Authentication failed." + task.getException(),
                                     Toast.LENGTH_SHORT).show();
                         } else {
-                            Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
-                            startActivity(intent);
+                            //Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                            //startActivity(intent);
+                            Log.d(TAG, "Authentication successful");
+
                         }
                     }
                 });
 
+    }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+
+                    DatabaseReference myRootRef = FirebaseDatabase.getInstance().getReference();
+                    DatabaseReference firstNameRef =  myRootRef.child("First Name");
+                    firstNameRef.setValue(firstName);
+                    DatabaseReference lastNameRef =  myRootRef.child("Last Name");
+                    lastNameRef.setValue(lastName);
+                    DatabaseReference accountTypeRef =  myRootRef.child("Account Type");
+                    accountTypeRef.setValue(accountType);
+
+
+                    Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        auth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            auth.removeAuthStateListener(mAuthListener);
+        }
     }
 //});
 //        }
